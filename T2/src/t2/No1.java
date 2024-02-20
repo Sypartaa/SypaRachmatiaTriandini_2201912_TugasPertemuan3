b81b59ebe3b4b2fbb83daf3ef84749867c03992f
Sypa Rachmatia Triandini <144791179+Sypartaa@users.noreply.github.com>
2024-02-20 11:34:08 +0700

    Update No1.java

--- T2/src/t2/No1.java
+++ T2/src/t2/No1.java
@@ -10,40 +10,32 @@ public class No1 {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-        // Input Pecahan Pertama
+        // Input Nilai Pecahan 
         System.out.print("Masukkan Pembilang Pecahan Pertama: ");
         int pembilang1 = userInput.nextInt();
         System.out.print("Masukkan Pembagi Pecahan Pertama (tidak boleh 0): ");
         int pembagi1 = userInput.nextInt();
-
-        // Input Pecahan Kedua
         System.out.print("Masukkan Pembilang Pecahan Kedua: ");
         int pembilang2 = userInput.nextInt();
         System.out.print("Masukkan Pembagi Pecahan Kedua (tidak boleh 0): ");
         int pembagi2 = userInput.nextInt();
-
-        // Input Pecahan Ketiga
         System.out.print("Masukkan Pembilang Pecahan Ketiga: ");
         int pembilang3 = userInput.nextInt();
         System.out.print("Masukkan Pembagi Pecahan Ketiga (tidak boleh 0): ");
         int pembagi3 = userInput.nextInt();
 
-        // Operasi Penjumlahan
+        // Operasi Pecahan
         double hasilPenjumlahan = (double) (pembilang1 * pembagi2 * pembagi3 + pembilang2 * pembagi1 * pembagi3 + pembilang3 * pembagi1 * pembagi2) / (pembagi1 * pembagi2 * pembagi3);
-        System.out.printf("Hasil Penjumlahan = %.2f\n", hasilPenjumlahan);
-
-        // Operasi Pengurangan
         double hasilPengurangan = (double) (pembilang1 * pembagi2 * pembagi3 - pembilang2 * pembagi1 * pembagi3 - pembilang3 * pembagi1 * pembagi2) / (pembagi1 * pembagi2 * pembagi3);
-        System.out.printf("Hasil Pengurangan = %.2f\n", hasilPengurangan);
-
-        // Operasi Perkalian
         double hasilPerkalian = (double) (pembilang1 * pembilang2 * pembilang3) / (pembagi1 * pembagi2 * pembagi3);
-        System.out.printf("Hasil Perkalian = %.2f\n", hasilPerkalian);
-
-        // Operasi Pembagian
         double hasilPembagian = (double) (pembilang1 * pembagi2 * pembagi3) / (pembilang2 *pembagi1 *  pembilang3);
-        System.out.printf("Hasil Pembagian = %.2f\n", hasilPembagian);
         
+        // Output
+        System.out.printf("Hasil Penjumlahan = %.2f\n", hasilPenjumlahan);
+        System.out.printf("Hasil Pengurangan = %.2f\n", hasilPengurangan);
+        System.out.printf("Hasil Perkalian = %.2f\n", hasilPerkalian);
+        System.out.printf("Hasil Pembagian = %.2f\n", hasilPembagian);
+         
         userInput.close();
     }
 }
